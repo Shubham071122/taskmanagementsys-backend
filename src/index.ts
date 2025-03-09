a9eea27f5a9e90c41846000554f335c6c78ffd9f
Shubham <shubhamkumar7112002@gmail.com>
2025-03-09 14:30:48 +0530

    client url added

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://taskmanagementsys.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
